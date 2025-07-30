d2f5504c43425de315ea1f163db70e1f09e87b58
Eduardo Barros <edu.barros.dev@gmail.com>
2025-07-30 00:07:03 -0300

    Task 3 Retangulo

---
+++ Heranca/retangulo/FormaGeometrica.java
@@ -0,0 +1,9 @@
+import java.lang.UnsupportedOperationException;
+
+public class FormaGeometrica {
+
+    public double area() {
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+
+}
