a8687b4f49c1bca2e77f439d1d6237b04b787726
Eduardo Barros <edu.barros.dev@gmail.com>
2025-07-29 23:40:35 -0300

    Task 1 Salario

---
+++ Heranca/salario/Empregado.java
@@ -0,0 +1,27 @@
+public class Empregado {
+
+    protected double salarioFixo;
+
+    public Empregado(double salarioFixo) {
+        this.salarioFixo = salarioFixo;
+    }
+
+    public double getSalarioFixo() {
+        return salarioFixo;
+    }
+
+    public double calcularBonus(Departamento departamento) {
+
+        if (departamento.alcancouMeta()) {
+            return salarioFixo * 0.1;
+        } else {
+            return 0;
+        }
+
+    }
+
+    public double calcularSalarioTotal(Departamento departamento) {
+        return salarioFixo + calcularBonus(departamento);
+    }
+
+}
