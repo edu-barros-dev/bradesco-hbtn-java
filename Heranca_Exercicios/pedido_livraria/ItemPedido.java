e9ac34d1608b8cea7a90659a74dad6e536fa76a3
Eduardo Barros <edu.barros.dev@gmail.com>
2025-08-01 00:02:50 -0300

    Task 0 Pedido Livraria - Exercicios Fixacao

---
+++ Heranca_Exercicios/pedido_livraria/ItemPedido.java
@@ -0,0 +1,27 @@
+public class ItemPedido {
+
+    private int quantidade;
+    private Produto produto;
+
+    public ItemPedido (Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+}
