2e4657d3fcdc3f08488dcfa9b144436f6bf5f53c
Eduardo Barros <edu.barros.dev@gmail.com>
2025-09-04 23:13:17 -0300

    Task 0 Blog - Collections - Exercícios de fixação

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Blog {
+
+    private ArrayList<Post> postagens;
+
+    public Blog() {
+        this.postagens = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post postagem) {
+        this.postagens.add(postagem);
+    }
+
+    public Set<String> obterTodosAutores() {
+        Set<String> autores = new TreeSet<>();
+        for (Post postagem : postagens) {
+            autores.add(postagem.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria () {
+
+        Map<String, Integer> contagem = new TreeMap<>();
+
+        for (Post p : postagens) {
+            String categoria = p.getCategoria();
+            if (contagem.containsKey(categoria)) {
+                contagem.put(categoria, contagem.get(categoria) + 1);
+            } else {
+                contagem.put(categoria, 1);
+            }
+        }
+
+        return contagem;
+
+    }
+
+}
