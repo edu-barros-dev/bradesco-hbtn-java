35ed0ad628ed5bb3a340e1a1802956bfc59afdbc
Eduardo Barros <edu.barros.dev@gmail.com>
2025-09-16 23:58:15 -0300

    Task 1 Consultar Produtos com Predicado - Lambda Expressions - Exercícios de fixação

---
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ConsultaProdutos {
+
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> criterio) {
+
+        return produtos.stream()
+                .filter(criterio::test)
+                .collect(Collectors.toList());
+
+    }
+
+}
